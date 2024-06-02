30d7a3f060604aba250b16235bf8e7513c889e32
Vraj Shah <vr597381@dal.ca>
2024-06-02 04:22:06 -0300

    feat: Add landing page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,27 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css';
-import Login from './pages/login/Login';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { LANDING, LOGIN } from "./utils/Routes";
+import { Suspense, lazy } from "react";
+import { Spinner } from "react-bootstrap";
 
-function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-      </Routes>
-  </Router>
-  );
+// Keep adding all screens here for lazy loading
+const LandingPage = lazy(() => import("./pages/landing_page/LandingPage"));
+const Login = lazy(() => import("./pages/login/Login"));
+
+export default function App() {
+    const Loader = () => (
+        <div className="d-flex align-items-center justify-content-center vh-100">
+            <Spinner animation="border" variant="primary" size="lg" />
+        </div>
+    );
+
+    return (
+        <BrowserRouter>
+            <Suspense fallback={<Loader />}>
+                <Routes>
+                    <Route path={LANDING} element={<LandingPage />} />
+                    <Route path={LOGIN} element={<Login />} />
+                </Routes>
+            </Suspense>
+        </BrowserRouter>
+    );
 }
-
-export default App;
