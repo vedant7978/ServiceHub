d271db85f94afa1b4740f9a8fd0c19d8bfb9bdba
Vraj Shah <vr597381@dal.ca>
2024-07-20 02:37:04 -0300

    refactor: Code quality improvement in Uploads module

--- backend/src/main/java/com/dalhousie/servicehub/controller/FileUploadController.java
+++ backend/src/main/java/com/dalhousie/servicehub/controller/FileUploadController.java
@@ -5,10 +5,9 @@ import com.dalhousie.servicehub.model.UserModel;
 import com.dalhousie.servicehub.response.FileUploadResponse;
 import com.dalhousie.servicehub.service.file_upload.FileUploadService;
 import com.dalhousie.servicehub.util.ResponseBody;
+import lombok.RequiredArgsConstructor;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,19 +17,14 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import static com.dalhousie.servicehub.util.ResponseBody.ResultType.FAILURE;
-import static com.dalhousie.servicehub.util.ResponseBody.ResultType.SUCCESS;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api")
 public class FileUploadController {
 
     private static final Logger logger = LogManager.getLogger(FileUploadController.class);
     private final FileUploadService fileUploadService;
-
-    @Autowired
-    public FileUploadController(FileUploadService fileUploadService) {
-        this.fileUploadService = fileUploadService;
-    }
 
     @PostMapping("/upload")
     public ResponseEntity<ResponseBody<FileUploadResponse>> handleFileUpload(
@@ -39,18 +33,17 @@ public class FileUploadController {
     ) {
         try {
             logger.info("Uploading file: {}", file.getOriginalFilename());
-            FileUploadResponse fileUploadResponse = fileUploadService.saveFile(userModel.getId(), file);
-            logger.info("File uploaded successfully: {}", fileUploadResponse);
-            ResponseBody<FileUploadResponse> body = new ResponseBody<>(SUCCESS, fileUploadResponse, "File uploaded successfully");
-            return ResponseEntity.status(HttpStatus.CREATED).body(body);
+            ResponseBody<FileUploadResponse> body = fileUploadService.saveFile(userModel.getId(), file);
+            logger.info("File uploaded successfully: {}", body.data().getUrl());
+            return ResponseEntity.ok(body);
         } catch (FileUploadException exception) {
             logger.error("Failed to upload file: {}", exception.getMessage());
             ResponseBody<FileUploadResponse> body = new ResponseBody<>(FAILURE, null, exception.getMessage());
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(body);
+            return ResponseEntity.badRequest().body(body);
         } catch (Exception exception) {
             logger.error("Error occurred while uploading file: {}", exception.getMessage());
             ResponseBody<FileUploadResponse> body = new ResponseBody<>(FAILURE, null, exception.getMessage());
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(body);
+            return ResponseEntity.badRequest().body(body);
         }
     }
 }
